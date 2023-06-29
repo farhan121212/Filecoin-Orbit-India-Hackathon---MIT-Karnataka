823e00c07cb0d07b995d6be73dfeeb1b7c155aed
FARHAN <farhan472003@gmail.com>
2023-06-29 20:27:54 +0530

    final files

---
+++ lib/data/stock.dart
@@ -0,0 +1,29 @@
+import '../models/stock.dart';
+
+List<Stock> stockPortfolio = [
+  Stock(
+      symbol: 'ZOMATO',
+      name: 'Zomato',
+      price: '\₹80.55',
+      change: '2.50(+1.60%)',
+      color: '#ff0000',
+      iconUrl:
+          'https://assets.upstox.com/content/dam/aem-content-integration/assets/images/logos/ZOMATO/ZOMATO.png'),
+  Stock(
+      symbol: 'META',
+      name: 'Facebook',
+      price: '\₹110.14',
+      change: '4.50(+6.60%)',
+      color: '#3a86ff',
+      iconUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png'),
+
+  Stock(
+      symbol: 'RELIANCE',
+      name: 'Reliance',
+      price: '\₹175.66',
+      change: '2.20(+3.60%)',
+      color: '#E7B10A',
+      iconUrl:
+          'https://assets.upstox.com/content/dam/aem-content-integration/assets/images/logos/RELIANCE/RELIANCE.png'),
+];
